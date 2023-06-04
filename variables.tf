731ac62be880c5bb471ebfe425cf8118a18ec450
Les Aker <me@lesaker.org>
2023-06-04 19:45:40 -0400

    bump source

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "lambda_version" {
   description = "Version of the Lambda to use"
   type        = string
-  default     = "v0.0.8"
+  default     = "v0.0.9"
 }
 
 variable "logging_bucket" {
